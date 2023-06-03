244f28af54edec0aa295d111bd3c5a119d47e6a2
netblock <19786740+netblock@users.noreply.github.com>
2023-06-02 19:19:05 -0500

    more srach

---
+++ sratch/spookymacro.c
@@ -0,0 +1,26 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct test {
+	int a;
+	int b;
+	int c;
+};
+
+#define settest(ppa,ppb,ppc) {.a = ppa, .b=ppb, .c=ppc}
+#define testplatform(args) settest args
+#define spooks(s,v, pptest) s = (struct test) pptest##platform(v) ;
+
+int main() {
+	struct test ttt;
+	spooks(ttt, 
+		(1,2,3),
+		test
+	)
+
+	printf("%i %i %i\n", ttt.a, ttt.b, ttt.c);
+}
+
